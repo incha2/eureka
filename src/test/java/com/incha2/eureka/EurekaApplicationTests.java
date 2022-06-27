9314ebf44f2e583ba58ab232277d41279a0e984e
Indrajit Chakraborty <indrachakraborty20@gmail.com>
2022-06-27 17:47:59 +0530

    First commit
    First commit
    
    Co-Authored-By: Indrajit Chakraborty <indrachakraborty20@gmail.com>

---
+++ src/test/java/com/incha2/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incha2.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
